fda6f7093e259e61559b2c8a1c4c9e471a1f977c
Hangovver <emirozdemir1407@gmail.com>
2025-10-07 22:09:47 +0300

    Update telegram.py

--- tools/telegram.py
+++ tools/telegram.py
@@ -5,17 +5,13 @@ from config import TELEGRAM_TOKEN, CHAT_ID
 def send_message(text):
     """Telegram’a mesaj gönderir"""
     if not TELEGRAM_TOKEN or not CHAT_ID:
-        print("⚠️ Telegram bilgileri eksik")
+        print("⚠️ Telegram bilgileri eksik.")
         return
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": text}
     try:
-        r = requests.post(url, data=data)
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        data = {"chat_id": CHAT_ID, "text": text}
+        r = requests.post(url, data=data, timeout=10)
         if r.status_code != 200:
             print(f"❌ Telegram hata: {r.text}")
     except Exception as e:
         print(f"❌ Telegram bağlantı hatası: {e}")
-
-
-
-
