d618d19c4de6ae2c019ef115b16d3363375b7f52
Hangovver <emirozdemir1407@gmail.com>
2025-10-07 21:53:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+# ⚙️ Temel yapılandırma ayarları
+
+# TheSportsDB API bilgileri
+API_KEY = "099583"  # Senin Premium key’in
+BASE_URL = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}"
+
+# 🔧 Telegram bilgileri
+TELEGRAM_TOKEN = "8227478980:AAGF1g0PHGp6j85SG8qPyyP9hYjgfJcWyRU"  # @BotFather’dan aldığın token
+CHAT_ID = "5876994093"  # kendi kullanıcı ID veya grup ID
